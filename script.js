986c4acf1a01073b135b6cd9d77a8d7eb6145c69
daiyanabdul <daiyanabdul1@gmail.com>
2025-01-31 21:49:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function highlight() {
-    //Write your code here
-
-
+    let boldWords = document.querySelectorAll('strong');
+    boldWords.forEach(function(word) {
+        word.style.color = 'green';
+    });
 }
 
-
 function return_normal() {
-    //Write your code here
-
-    
+    let boldWords = document.querySelectorAll('strong');
+    boldWords.forEach(function(word) {
+        word.style.color = 'black';
+    });
 }
